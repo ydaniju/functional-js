10541bc9bfab522942e1e61c76542a6635cd6566
ydaniju <danijuyusuf@outlook.com>
2020-05-19 07:40:37 +0100

    duplexer redux

--- stream-adventure/duplexer_redux.js
+++ stream-adventure/duplexer_redux.js
@@ -1,7 +1,19 @@
 const duplexer = require('duplexer2');
+const thru = require('through2').obj
 
 module.exports = function (counter) {
-  counter.on('pipe', (src) => {
-    counter.setCounts();
-  });
+  const counts = {};
+
+  const inputStream = thru(
+    function (row, _, next) {
+      counts[row.country] = (counts[row.country] || 0) + 1
+      next()
+    },
+    end => {
+      counter.setCounts(counts)
+      end()
+    },
+  )
+
+  return duplexer({ objectMode: true }, inputStream, counter);
 }
