be12faca40521bad552dd6a1e257fb07da7f49fd
ydaniju <danijuyusuf@outlook.com>
2020-06-21 20:57:26 +0100

    to resolve or not

---
+++ promise-it-wont-hurt/reject_or_not.js
@@ -0,0 +1,8 @@
+const promise = new Promise((fulfill, reject) => {
+  fulfill('I FIRED')
+  reject(new Error('I DID NOT FIRE'))
+})
+
+const onRejected = (err) => console.log(err.message)
+
+promise.then(console.log, onRejected)
